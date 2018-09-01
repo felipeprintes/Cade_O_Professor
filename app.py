2a94f4ce1a2285985de0ae31cb6891ef58a7d0b5
Bruno Landeiro <landeiro.b@gmail.com>
2018-09-01 16:33:27 -0300

    Perfil e disciplinas

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ from flask import Flask, render_template, request, url_for, redirect
 import json, requests
 app = Flask(__name__)
 
+class Api:
+    aluno_url = 'http://localhost:8080/api/v1/alunos/'
+    disciplina_url = 'http://localhost:8080/api/v1/disciplinas/'
+
+    def get_aluno_by_id(self, id):
+        url_param = self.aluno_url+"{}"
+        return url_param.format(id)
+
+    def get_disciplina_by_id(self, id):
+        url_param = self.disciplina_url+"{}"
+        return url_param.format(id)
+
+
+class Aluno:
+    def __init__(self,id, nome, email):
+	    self.id=id
+	    self.nome=nome
+	    self.email=email
+
 @app.route("/")
 @app.route("/index")
 def home():
@@ -16,18 +35,39 @@ def cadastro_usuario():
         email = request.form.get("email")
         senha = request.form.get("senha")
 
+        api = Api()
         dados = {"nome":nome, "email":email, "senha": senha}
-        response = requests.post("http://localhost:8080/api/v1/alunos/", dados)
-
-        if response.status_code == 200:
-            print("status true 200")
-            return "<h1>Cadastro realizado com sucesso</h1>"
+        response = requests.post(api.aluno_url, dados)
+        if response.ok:
+            json_response = json.loads(response.content)
+            
+            return redirect("/aluno/perfil/{}".format(json_response['response']['insertId']))
         else:
             return "<h1>Erro ao efetuar cadastro</h1>"
         
     else:
         return "não deu"
 
+
+@app.route("/aluno/perfil/<id>", methods=['GET'])
+def aluno_perfil(id):
+    api = Api()
+    response = requests.get(api.get_aluno_by_id(id))
+    
+    if response.status_code == 200:
+        json_response = json.loads(response.content)
+        id_nome = json_response['response'][0]['id_aluno']
+        nome = json_response['response'][0]['nome']
+        email = json_response['response'][0]['email']
+        
+        aluno = Aluno(id_nome,nome,email)
+        response_disciplinas = requests.get(api.disciplina_url)
+        json_disciplinas = json.loads(response_disciplinas.content)
+
+        return render_template('perfil.html',aluno=aluno, disciplinas=json_disciplinas['response'])
+
+    else:
+        return "<h1>perfil não encontrado</h1>"
 
 @app.route("/aluno/login", methods=['GET', 'POST'])
 def aluno_login():
@@ -43,6 +83,20 @@ def aluno_login():
     else:
         return "<h1>Usuario não encontrado"
 
+
+@app.route("/disciplina/", methods=['GET'])
+def disciplina():
+    api = Api()
+    disciplina = request.args["id"]
+    response = requests.get(api.get_disciplina_by_id(disciplina))
+    
+    if response.ok:
+        json_response = json.loads(response.content)
+
+        return render_template('disciplina.html', disciplina=json_response['response'][0])
+
+    else:
+        return "<h1>disciplina não encontrada</h1>"
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
